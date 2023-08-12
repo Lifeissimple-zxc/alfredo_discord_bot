f96ca0f15ebcb1633b13d1546620b7ad9f49ea50
Lifeissimple-zxc <mararkarp@gmail.com>
2023-08-12 12:26:13 +0200

    Added schemas for transaction data to models and user inputs

--- alfredo_lib/local_persistence/models.py
+++ alfredo_lib/local_persistence/models.py
@@ -2,25 +2,22 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import (
     Column,
     Integer,
-    String
+    String,
+    Float,
+    ForeignKey
 )
-
-# This is an ORM that is responsible for all the operations with the local sqlite3 db.
-# It needs to support the following functions:
-# Add new row to users table
-# Update data in users table
-# Add transaction row to the table
-# Drop row in the transaction table
-
 
 # Define our tables
 # First we need a base to access metadata about schema
 Base = declarative_base()
 
-# Table classes
 class User(Base):
+    """
+    ### Class models user data powering alferdo
+    """
     # Tablename
     __tablename__ = "users"
+
     # User Attributes
     user_id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True, nullable=False)
@@ -30,7 +27,35 @@ class User(Base):
     currency = Column(String(10))
     spreadsheet = Column(String(50))
 
+class Transaction(Base):
+    """
+    ### Class models transaction rows
+    """
+    __tablename__ = "transaction_cache"
+
+    transaction_id = Column(Integer, primary_key=True)
+    created = Column(Integer, nullable=False)
+    # user_id as FK
+    user_id = Column(
+        Integer,
+        ForeignKey("users.user_id",ondelete="CASCADE"),
+        nullable=False
+    )
+    amount = Column(Float(precision=4), nullable=False)
+    # Making it three here because of ISO 4217
+    # Ideally, I should not be storing text, but model currencies as a separate table :)
+    # For comment text is the only option
+    currency = Column(String(3), nullable=False)
+    category = Column(String(30), nullable=False)
+    comment = Column(String(100))
+    split_percent = Column(Float(precision=2))
+    # This field needs to update every time users update a transaction
+    updated_at = Column(Integer, nullable=False)
+
 class LogRecordRow(Base):
+    """
+    ### Class models logrecords
+    """
     # Dst table for all log records
     __tablename__ = "logs"
     # Logging datapoints

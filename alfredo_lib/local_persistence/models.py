e2bc7b1cfe26729bdf2af738c03c72249bd0eba3
Lifeissimple-zxc <mararkarp@gmail.com>
2023-10-17 23:59:49 +0200

    Updated ORM models to correctly model relationsips between transactions and categories

--- alfredo_lib/local_persistence/models.py
+++ alfredo_lib/local_persistence/models.py
@@ -16,7 +16,6 @@ class User(Base):
     ### Models user data powering alferdo
     """
     __tablename__ = "users"
-
     # User Attributes
     user_id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True, nullable=False)
@@ -25,14 +24,14 @@ class User(Base):
     created = Column(Integer, nullable=False)
     currency = Column(String(10))
     spreadsheet = Column(String(50))
+    transactions = relationship("Transaction", back_populates="user")
 
 
 class Transaction(Base):
     """
     ### Models transaction rows
     """
-    __tablename__ = "transaction"
-
+    __tablename__ = "transactions"
     transaction_id = Column(Integer, primary_key=True)
     created = Column(Integer, nullable=False)
     # user_id as FK
@@ -43,7 +42,7 @@ class Transaction(Base):
     )
     amount = Column(Float(precision=FLOAT_PRECISION), nullable=False)
     # Making it three here because of ISO 4217
-    # Ideally, I should not be storing text, but model currencies as a separate table :)
+    # Ideally, I should model currencies as a separate table :)
     # For comment text is the only option
     currency = Column(String(3), nullable=False)
     category_id = Column(Integer,
@@ -53,10 +52,19 @@ class Transaction(Base):
     split_percent = Column(Float(precision=FLOAT_PRECISION))
     # This field needs to update every time users update a transaction
     updated_at = Column(Integer, nullable=False)
-    
-    # Relationships
-    user = relationship("User", backref="transaction")
-    category = relationship("Category", backref="transaction")
+    category = relationship("Category", back_populates="transactions")
+    user = relationship("User", back_populates="transactions")
+
+
+class Category(Base):
+    """
+    ### Models transaction categories
+    """
+    __tablename__ = "categories"
+    category_id = Column(Integer, primary_key=True)
+    created = Column(Integer, nullable=False)
+    category_name = Column(String(100), nullable=False, unique=True)
+    transactions = relationship("Transaction", back_populates="category")
 
 
 class LogRecord(Base):
@@ -72,13 +80,3 @@ class LogRecord(Base):
     message = Column(String(300), nullable=False)
     level = Column(String(30), nullable=False)
     func_name = Column(String(30), nullable=False)
-
-
-class Category(Base):
-    """
-    ### Models transaction categories
-    """
-    __tablename__ = "categories"
-    category_id = Column(Integer, primary_key=True)
-    created = Column(Integer, nullable=False)
-    category_name = Column(String(100), nullable=False)
